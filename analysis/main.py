a9fc626604c95121f683b5a2ca193685649a9962
Ugo Tanielian <ugo.tanielian@outlook.fr>
2019-03-21 19:27:15 +0100

    new files main

---
+++ analysis/main.py
@@ -0,0 +1,9 @@
+
+
+from source.read_in_and_cleaning import load_data_and_clean
+
+
+
+
+data = load_data_and_clean()
+print(data["body_c"][999])
--- analysis/source/read_in_and_cleaning.py
+++ analysis/source/read_in_and_cleaning.py
@@ -1,41 +1,43 @@
 ## Python Setup
 
 import pandas as pd
-from text_analysis import create_bag_of_words, create_topics, get_word_counts
+from source.text_analysis import create_bag_of_words, create_topics, get_word_counts
 
 
-## Data Read In
+def load_data_and_clean():
+    ## Data Read In
 
-df = pd.read_csv('../data/raw_kaggle_data/data.csv')
-df.columns = [col.lower() for col in list(df)]
-# df.head()
-# df.shape
-
-
-## Data Cleaning
-
-df['body_c'] = df['body']
-
-# Special Text Sequences
-df['body_c'] = df['body_c'].str.replace(r'^[A-Z\s]+\s\(Reuters\)\s\-\s', '')
-df['body_c'] = df['body_c'].str.replace(r'\bFILE\sPHOTO\:\s.+\n\(Reuters\)\s\-\s', '')
-df['body_c'] = df['body_c'].str.replace(r'\(Reuters\)\s\-\s', '')
-
-# Classic Cleaning
-df['body_c'] = df['body_c'].str.replace(r'\(.+\)', '')
-df['body_c'] = df['body_c'].str.replace(r'\s+', ' ')
-df['body_c'] = df['body_c'].str.replace(r'\n', ' ')
-df['body_c'] = df['body_c'].str.replace(r'\'', '’')
-df['body_c'] = df['body_c'].str.strip()
+    df = pd.read_csv('data/raw_kaggle_data/data.csv')
+    df.columns = [col.lower() for col in list(df)]
+    # df.head()
+    # df.shape
 
 
-## Export
-df_dict = df.to_dict()
+    ## Data Cleaning
+
+    df['body_c'] = df['body']
+
+    # Special Text Sequences
+    df['body_c'] = df['body_c'].str.replace(r'^[A-Z\s]+\s\(Reuters\)\s\-\s', '')
+    df['body_c'] = df['body_c'].str.replace(r'\bFILE\sPHOTO\:\s.+\n\(Reuters\)\s\-\s', '')
+    df['body_c'] = df['body_c'].str.replace(r'\(Reuters\)\s\-\s', '')
+
+    # Classic Cleaning
+    df['body_c'] = df['body_c'].str.replace(r'\(.+\)', '')
+    df['body_c'] = df['body_c'].str.replace(r'\s+', ' ')
+    df['body_c'] = df['body_c'].str.replace(r'\n', ' ')
+    df['body_c'] = df['body_c'].str.replace(r'\'', '’')
+    df['body_c'] = df['body_c'].str.strip()
 
 
-## Sandbox
+    ## Export
+    df_dict = df.to_dict()
 
-# n=234
-# print(df['body'][n])
-# print('\n----------\n')
-# print(df['body_c'][n])
+
+    ## Sandbox
+
+    # n=234
+    # print(df['body'][n])
+    # print('\n----------\n')
+    # print(df['body_c'][n])
+    return df_dict

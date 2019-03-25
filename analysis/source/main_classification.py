6a359116fa201289a9c8786b47a26b41d2c48148
Ugo Tanielian <ugo.tanielian@outlook.fr>
2019-03-25 17:25:40 +0100

    c

--- analysis/source/main_classification.py
+++ analysis/source/main_classification.py
@@ -1,1 +1,57 @@
 import tensorflow as tf
+
+
+# There are n columns in the feature matrix 
+# after One Hot Encoding. 
+X = tf.placeholder(tf.float32, [None, n]) 
+Y = tf.placeholder(tf.float32, [None, 2]) 
+W = tf.Variable(tf.zeros([n, 2])) 
+b = tf.Variable(tf.zeros([2])) 
+
+# Hypothesis 
+Y_hat = tf.nn.sigmoid(tf.add(tf.matmul(X, W), b)) 
+cost = tf.nn.sigmoid_cross_entropy_with_logits(logits = Y_hat, labels = Y) 
+  
+# Gradient Descent Optimizer 
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = alpha).minimize(cost) 
+  
+# Global Variables Initializer 
+init = tf.global_variables_initializer() 
+ 
+# Starting the Tensorflow Session 
+with tf.Session() as sess: 
+
+    # Initializing the Variables 
+    sess.run(init) 
+      
+    # Lists for storing the changing Cost and Accuracy in every Epoch 
+    cost_history, accuracy_history = [], [] 
+      
+    # Iterating through all the epochs 
+    for epoch in range(epochs): 
+        cost_per_epoch = 0
+          
+        # Running the Optimizer 
+        sess.run(optimizer, feed_dict = {X : x, Y : y})           
+        c = sess.run(cost, feed_dict = {X : x, Y : y}) 
+          
+        # Calculating accuracy on current Epoch 
+        correct_prediction = tf.equal(tf.argmax(Y_hat, 1), tf.argmax(Y, 1)) 
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+          
+        # Storing Cost and Accuracy to the history 
+        cost_history.append(sum(sum(c))) 
+        accuracy_history.append(accuracy.eval({X : x, Y : y}) * 100) 
+          
+        # Displaying result on current Epoch 
+        if epoch % 100 == 0 and epoch != 0: 
+            print("Epoch " + str(epoch) + " Cost: "
+                            + str(cost_history[-1])) 
+      
+    Weight = sess.run(W) # Optimized Weight 
+    Bias = sess.run(b)   # Optimized Bias 
+      
+    # Final Accuracy 
+    correct_prediction = tf.equal(tf.argmax(Y_hat, 1), tf.argmax(Y, 1)) 
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+    print("\nAccuracy:", accuracy_history[-1], "%") 

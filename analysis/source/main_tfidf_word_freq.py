eb83773b7aff4647f07948fbfcf78bdff7b50f48
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2019-04-08 17:14:59 -0400

    updates to tfidf stuff

--- analysis/source/main_tfidf_word_freq.py
+++ analysis/source/main_tfidf_word_freq.py
@@ -1,9 +1,9 @@
-from models.tfidf_word_freq import tf_idf_word_freq
+from models.tfidf import tf_idf
 
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 
-tf_idf_word_freq('body', min_df=0.1, max_df=0.9, stop_words=stop_words, stem=True, ngram_range=(1,2), use_idf=True)
-tf_idf_word_freq('headline', min_df=0.01, max_df=0.95, stop_words=stop_words, stem=True, ngram_range=(1,2), use_idf=True)
+tf_idf('body', min_df=0.1, max_df=0.9, stop_words=stop_words, stem=True, ngram_range=(1,2), use_idf=True)
+tf_idf('headline', min_df=0.01, max_df=0.95, stop_words=stop_words, stem=True, ngram_range=(1,2), use_idf=True)

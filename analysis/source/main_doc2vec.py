88125b284c695c3940d842f5206c8d64e1fc07dd
Nicolas Guetta Jeanrenaud <nicolas.jeanrenaud@gmail.com>
2019-04-19 16:37:32 -0400

    increased flexibility in parameters

--- analysis/source/main_doc2vec.py
+++ analysis/source/main_doc2vec.py
@@ -1,5 +1,8 @@
 from models.doc2vec_param import Param
 from models.doc2vec import create_and_train_doc2vec_model
 
-param = Param()
+param = Param(text_var='body')
+create_and_train_doc2vec_model(param)
+
+param = Param(text_var='headline')
 create_and_train_doc2vec_model(param)
